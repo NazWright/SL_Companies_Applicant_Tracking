021b5eee50241761ce0fbb18d962d5eb5d1ed962
Nazere Wright <nazwrightthedeveloper@gmail.com>
2021-08-21 18:17:18 -0400

    added test for API creating job listings

--- test/controller/test_applicantTrackingController.js
+++ test/controller/test_applicantTrackingController.js
@@ -26,6 +26,34 @@ describe("Testing applicant tracking controller", () => {
   });
 
   it("Should create a job listing", () => {
-    const response = await server.post("/api/jobs/create").send({});
+    // pay header for the job will have to have the lowest min between the two and the highest max
+    const morningShift = {name:'1st', pay:{}, startTime: '', endTime: ''}
+    const eveningShift = {name:'2nd', pay:{}, startTime: '', endTime: ''}
+    const availWeekends, availHolidays, availOvertime, avail8To12HR = true; 
+    const tasks, skills = {};
+    const responsibilites, equipmentNeeded = [];
+    const location, jobType = "";
+    const preferredRadius = 0;
+    const publishedDate = Date.now();
+    const response = await server.post("/api/jobs/create").send({
+        title: "Furniture Frame Builder",
+        shifts: [morningShift, eveningShift],
+        schedule: {
+            avail8To12HR,
+            availHolidays,
+            availOvertime,
+            availWeekends
+       },
+       tasks,
+       skills,
+       responsibilites,
+       equipmentNeeded,
+       location,
+       publishedDate,
+       jobType,
+       preferredRadius,
+    });
+    // assert that the response and the created date are the same
+    assert(response.body._id && response.body.publishedDate);
   });
 });
